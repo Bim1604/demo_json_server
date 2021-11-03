17ef03412b21395d3e8dea1d95c676a32d5db0b9
bimz <dangvipro3@gmail.com>
2021-11-03 14:30:22 +0700

    modify data user

--- generate-data.js
+++ generate-data.js
@@ -127,7 +127,7 @@ const randomProductList = (categoryList, numberOfProducts) => {
           'https://scontent.fsgn2-5.fna.fbcdn.net/v/t31.18172-8/26685354_1945336242460292_7151551022183058927_o.jpg?_nc_cat=104&ccb=1-5&_nc_sid=174925&_nc_ohc=hJf4qGXVbS4AX8dJmLH&_nc_ht=scontent.fsgn2-5.fna&oh=c9cc310e5beb2694dfafac2de75c2057&oe=61A52F70',
         gender: 'Nam',
         DOB: '16/04/2000',
-        phone: '0852573133',
+        phone: '0852573132',
         password: '123',
         role: "cus",
       },
